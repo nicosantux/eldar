52e8dd8bdd10baa887816904d52e07778d24781e
Nicolás Santuccio <nico.santuccio@gmail.com>
2024-10-18 14:46:58 -0300

    chore: add mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import CssBaseline from '@mui/material/CssBaseline'
+
 import App from './app.tsx'
-import './index.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 )
